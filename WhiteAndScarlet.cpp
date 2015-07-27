ba410c99765e9d1ecc371302ebbff9b61972c4c2
artylo <artylojunk@abv.bg>
2015-07-27 22:36:30 +0300

    trying out header files

--- WhiteAndScarlet.cpp
+++ WhiteAndScarlet.cpp
@@ -1,52 +1,10 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-#include <string>
-#include <cstring>
-using namespace std;
+#include "head.h"
 
 /*GLOBALS*/
 const int players = 4;
 const int tiles   = 5;
 const int townNum   = tiles * 20/100;
 const int castleNum = tiles * 30/100;
-
-/*STRUCTURES*/
-struct GENERAL
-{
-    string name;
-    int stars = 2;
-    int army  = 100;
-    int x[5] = {0,0,0,0,0}; //Infantry//Cavalry//Archers//Artillery//Ships
-};
-struct PLAYER
-{
-    string name;
-    GENERAL general[4];
-    int gold = 0;
-
-};
-struct TOWN
-{
-    string name   = " ";
-    string owner  = " ";
-    int armour    = 1;
-    //int capacity  = 800;
-    int income    = 400;
-    //int training = 100;
-    //int trainCost = 2;
-};
-struct CASTLE
-{
-    string name  = " ";
-    string owner = " ";
-    int armour = 2;
-    //int capacity = 200;
-    //int income   = 40;
-    int training = 100;
-    int trainCost = 2;
-};
 
 /*FUNCTIONS*/
 void set_owner_castle(CASTLE castle[])
---
+++ crown.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
+
+/*STRUCTURES*/
+struct GENERAL
+{
+    string name;
+    int stars = 2;
+    int army  = 100;
+    int x[5] = {0,0,0,0,0}; //Infantry//Cavalry//Archers//Artillery//Ships
+};
+struct PLAYER
+{
+    string name;
+    GENERAL general[4];
+    int gold = 0;
+
+};
+struct TOWN
+{
+    string name   = " ";
+    string owner  = " ";
+    int armour    = 1;
+    //int capacity  = 800;
+    int income    = 400;
+    //int training = 100;
+    //int trainCost = 2;
+};
+struct CASTLE
+{
+    string name  = " ";
+    string owner = " ";
+    int armour = 2;
+    //int capacity = 200;
+    //int income   = 40;
+    int training = 100;
+    int trainCost = 2;
+};
